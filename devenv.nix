93683b9e596c92c9dd664e3fca8057af45dd15b4
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-28 22:08:12 +0200

    ...

--- devenv.nix
+++ devenv.nix
@@ -1,42 +1,48 @@
 {
-  pkgs, lib, config, inputs, ...
-}:
-
-{
-  env = {
-    GREET = "devenv";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    devenv.url = "github:cachix/devenv";
   };
 
-  packages = [ pkgs.git ];
+  outputs = { self, nixpkgs, devenv, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    devenv.lib.mkDevEnv {
+      name = "my-dev-env";
 
-  enterShell = ''
-    git --version
-    python -V
-    cat << "EOF"
-    _______  ________  ________  ________  ________  ________
-  _╱       ╲╱        ╲╱    ╱   ╲╱        ╲╱    ╱   ╲╱    ╱   ╲
- ╱         ╱         ╱         ╱         ╱         ╱         ╱
-╱         ╱        _╱╲        ╱        _╱         ╱╲        ╱
-╲________╱╲________╱  ╲______╱╲________╱╲__╱_____╱  ╲______╱
+      languages.python = {
+        enable = true;
+        version = "3.10";
+        venv = {
+          enable = true;
+          requirements = ''
+            pyserial
+            gps3
+            obd
+          '';
+        };
+      };
 
-EOF
-  '';
+      packages = [
+        pkgs.git
+      ];
 
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep "2.42.0"
-  '';
+      shellHook = ''
+        export GREET="devenv"
+        echo "Environment variable GREET is set to $GREET"
+        echo "Python version: $(python -V)"
+        echo "Git version: $(git --version)"
+        cat << "EOF"
+        _______  ________  ________  ________  ________  ________
+      _╱       ╲╱        ╲╱    ╱   ╲╱        ╲╱    ╱   ╲╱    ╱   ╲
+     ╱         ╱         ╱         ╱         ╱         ╱         ╱
+    ╱         ╱        _╱╲        ╱        _╱         ╱╲        ╱
+    ╲________╱╲________╱  ╲______╱╲________╱╲__╱_____╱  ╲______╱
+    EOF
 
-  languages.python = {
-    enable = true;
-    version = "3.12";
-    venv = {
-      enable = true;
-      requirements = ''
-        pyserial
-        gps3
-        obd
+        echo "Running tests"
+        git --version | grep "2.42.0"
       '';
     };
-  };
 }
