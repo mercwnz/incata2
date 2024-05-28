4120d911da3b3cb1998544d0f3e561cb050df8b6
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-28 22:11:45 +0200

    ...

--- devenv.nix
+++ devenv.nix
@@ -1,48 +1,42 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
-    devenv.url = "github:cachix/devenv";
+  pkgs, lib, config, inputs, ...
+}:
+
+{
+  env = {
+    GREET = "devenv";
   };
 
-  outputs = { self, nixpkgs, devenv, ... }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-    devenv.lib.mkDevEnv {
-      name = "my-dev-env";
+  packages = [ pkgs.git ];
 
-      languages.python = {
-        enable = true;
-        version = "3.10";
-        venv = {
-          enable = true;
-          requirements = ''
-            pyserial
-            gps3
-            obd
-          '';
-        };
-      };
+  enterShell = ''
+    git --version
+    python -V
+    cat << "EOF"
+    _______  ________  ________  ________  ________  ________
+  _╱       ╲╱        ╲╱    ╱   ╲╱        ╲╱    ╱   ╲╱    ╱   ╲
+ ╱         ╱         ╱         ╱         ╱         ╱         ╱
+╱         ╱        _╱╲        ╱        _╱         ╱╲        ╱
+╲________╱╲________╱  ╲______╱╲________╱╲__╱_____╱  ╲______╱
 
-      packages = [
-        pkgs.git
-      ];
+EOF
+  '';
 
-      shellHook = ''
-        export GREET="devenv"
-        echo "Environment variable GREET is set to $GREET"
-        echo "Python version: $(python -V)"
-        echo "Git version: $(git --version)"
-        cat << "EOF"
-        _______  ________  ________  ________  ________  ________
-      _╱       ╲╱        ╲╱    ╱   ╲╱        ╲╱    ╱   ╲╱    ╱   ╲
-     ╱         ╱         ╱         ╱         ╱         ╱         ╱
-    ╱         ╱        _╱╲        ╱        _╱         ╱╲        ╱
-    ╲________╱╲________╱  ╲______╱╲________╱╲__╱_____╱  ╲______╱
-    EOF
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
 
-        echo "Running tests"
-        git --version | grep "2.42.0"
+  languages.python = {
+    enable = true;
+    version = "3.12";
+    venv = {
+      enable = true;
+      requirements = ''
+        pyserial
+        gps3
+        obd
       '';
     };
+  };
 }
