45a33c3566dc4fb9d13a2c04079be8f2a27c6678
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-28 22:19:36 +0200

    ...

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  # Define the Python environment with required packages
+  pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+    ps.pyserial
+    ps.gps3
+    ps.obd
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.git
+    pythonEnv
+  ];
+
+  shellHook = ''
+    export GREET="devenv"
+    echo "Environment variable GREET is set to $GREET"
+    echo "Python version: $(python -V)"
+    echo "Git version: $(git --version)"
+    cat << "EOF"
+    _______  ________  ________  ________  ________  ________
+  _╱       ╲╱        ╲╱    ╱   ╲╱        ╲╱    ╱   ╲╱    ╱   ╲
+ ╱         ╱         ╱         ╱         ╱         ╱         ╱
+╱         ╱        _╱╲        ╱        _╱         ╱╲        ╱
+╲________╱╲________╱  ╲______╱╲________╱╲__╱_____╱  ╲______╱
+EOF
+
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+}
