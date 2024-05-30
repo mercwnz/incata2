bb6ce0190ac72b68eb3756962472af77000dfab7
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-30 23:38:19 +0200

    ..

--- test4.py
+++ test4.py
@@ -1,4 +1,18 @@
-import subprocess
+import subprocess, time
+from collections import deque
+
+def __init__(self):
+    self.temp_data = {}
+    self.last_sentence_type = ""
+    self.latitude_queue = deque(maxlen=5)
+    self.longitude_queue = deque(maxlen=5)
+    self.speed_queue = deque(maxlen=5)
+    self.direction_queue = deque(maxlen=5)
+    self.altitude_queue = deque(maxlen=5)
+    self.climb_queue = deque(maxlen=5)
+
+def moving_average(queue):
+    return sum(queue) / len(queue) if queue else 0
 
 def checksum(nmea):
     nmea = nmea.strip('$').split('*')[0]
@@ -8,26 +22,25 @@ def checksum(nmea):
     return checksum
 
 def read_gps_data():
-    # Start the gpspipe process
     process = subprocess.Popen(['gpspipe', '-r'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     try:
         while True:
-            # Read a line of output from gpspipe
             line = process.stdout.readline() # type: ignore
             if line:
                 line = line.strip()
                 if '*' in line:
                     data, checksum_str = line.split('*')
-                    checksum_val = int(checksum_str, 16)
-                    calculated_checksum = checksum(data)
-                    print(f"{checksum_val} {calculated_checksum} : {data}")
+                    if int(checksum_str, 16) == checksum(data):
+                        fields = data.split(',')
+                        print(f"{checksum_str} ; {fields[0][1:]}")
+                    else:
+                        raise ValueError("checksum error")
             else:
                 break
     except KeyboardInterrupt:
         print("Stopping GPS data read...")
     finally:
-        # Clean up the process
         process.terminate()
         process.wait()
 
