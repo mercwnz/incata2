2d051a6a49ee89b62f51c88ef731503fc16cb986
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-31 01:20:58 +0200

    ...

--- test5.py
+++ test5.py
@@ -4,6 +4,13 @@ import sqlite3
 
 class NMEA:
 
+    def get_cardinal_direction(self, degrees):
+        if degrees is None:
+            return "N/A"
+        dirs = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+        ix = round(degrees / 45) % 8
+        return dirs[ix]
+
     def read_gps_json(self):
         process = subprocess.Popen(['gpspipe', '-w'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
@@ -13,16 +20,17 @@ class NMEA:
                 if line:
                     try:
                         json_data = json.loads(line.strip())
-                        pretty_json = json.dumps(json_data, indent=4)
                         if json_data["class"] == "TPV":
                             lat = json_data.get('lat', 'N/A')
                             lon = json_data.get('lon', 'N/A')
                             speed = json_data.get('speed', 'N/A')
                             magtrack = json_data.get('magtrack', 'N/A')
-                            print(f"Latitude: {lat}")
-                            print(f"Longitude: {lon}")
-                            print(f"Speed: {speed}")
-                            print(f"Magtrack: {magtrack}")
+
+                            print(f"Latitude:   {lat}")
+                            print(f"Longitude:  {lon}")
+                            print(f"Speed:      {speed}")
+                            print(f"Magtrack:   {self.get_cardinal_direction(magtrack)} : {magtrack}°")
+
                         else:
                             print(f"{json_data['class']}")
                     except json.JSONDecodeError:
