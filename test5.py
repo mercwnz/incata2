1977edf0b98b22603c8ad2add329c26e2344353e
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-31 01:03:40 +0200

    ...

--- test5.py
+++ test5.py
@@ -1,23 +1,6 @@
-from nmea import input_stream, data_frame, database_wrapper
-
-stream = input_stream.GenericInputStream.open_stream('/dev/ttyACM0', 9600)
-
-
-print(stream)
-
-with stream:
-    while True:
-        new_frame = data_frame.DataFrame.get_next_frame(stream)
-
-        print("Current GPS time:", new_frame.gps_time)
-        print("Current Latitude:", new_frame.latitude)
-        print("Current Longitude:", new_frame.longitude)
-        print("Current Speed:", new_frame.velocity)
-        print("Current heading:", new_frame.track)
-        print("Number of Satellites above:", new_frame.nsats)
-        print("Individual Observations:")
-        for obs in new_frame.sv_observations:
-            print('\tPRN:', obs.prn)
-            print('\t\tSignal to Noise:', obs.snr)
-            print('\t\tAzimuth:', obs.azimuth)
-            print('\t\tElevation:', obs.elevation)
+from serial import Serial
+from pynmeagps import NMEAReader
+with Serial('/dev/ttyUSB0', 9600, timeout=3) as stream:
+  nmr = NMEAReader(stream)
+  raw_data, parsed_data = nmr.read()
+  print(parsed_data)
