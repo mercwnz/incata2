95d1a970c154503d064cd7aa921b8fd5b2a51561
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-31 01:41:20 +0200

    ...

--- test5.py
+++ test5.py
@@ -30,8 +30,8 @@ class NMEA:
                             print(f"Maps:       https://www.google.com/maps?q={lat},{lon}")
                             print(f"Latitude:   {lat}")
                             print(f"Longitude:  {lon}")
-                            print(f"Speed:      {round(speed * 3.6)} km/h")
-                            print(f"Magtrack:   {magtrack}°")
+                            print(f"Speed:      {round(speed) * 3.6} km/h")
+                            print(f"Magtrack:   {round(magtrack)}°")
                             print(f"Direction:  {self.get_cardinal_direction(magtrack)}")
                             print(f"\n")
 
