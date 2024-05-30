1650dee401f67b973f6737545a0b960845b097f3
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-30 23:18:11 +0200

    ...

--- test4.py
+++ test4.py
@@ -1,30 +1,24 @@
 import subprocess
-import threading
-import time
+
+def checksum(line):
+    line = line.strip('$').split('*')[0]
+    checksum = 0
+    for char in line:
+        checksum ^= ord(char)
+    return checksum
 
 def read_gps_data():
-    # Start the gpspipe process
     process = subprocess.Popen(['gpspipe', '-r'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-    def read_output(stream):
-        while not process.poll():
-            line = stream.readline()
-            if line.startswith('$'):
-                print(f"{line.strip()}")
-            else:
-                time.sleep(0.1)
-
-    stdout_thread = threading.Thread(target=read_output, args=(process.stdout,))
-    stderr_thread = threading.Thread(target=read_output, args=(process.stderr,))
-
-    stdout_thread.start()
-    stderr_thread.start()
-
     try:
-        stdout_thread.join()
-        stderr_thread.join()
+        while True:
+            line = process.stdout.readline() # type: ignore
+            if line.startswith("$"):
+                print(f"{checksum(line.strip())} : {line.strip()}")
+            else:
+                break
     except KeyboardInterrupt:
         print("Stopping GPS data read...")
+    finally:
         process.terminate()
         process.wait()
 
