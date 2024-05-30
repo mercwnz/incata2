7b4dc2aa372675922baf97212678e28231457183
Roy Betjes <roy.betjes@talpanetwork.com>
2024-05-30 23:21:31 +0200

    ...

--- test4.py
+++ test4.py
@@ -1,24 +1,28 @@
 import subprocess
 
-def checksum(line):
-    line = line.strip('$').split('*')[0]
+def checksum(nmea_sentence):
+    nmea_sentence = nmea_sentence.strip('$').split('*')[0]
     checksum = 0
-    for char in line:
+    for char in nmea_sentence:
         checksum ^= ord(char)
     return checksum
 
 def read_gps_data():
+    # Start the gpspipe process
     process = subprocess.Popen(['gpspipe', '-r'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
     try:
         while True:
+            # Read a line of output from gpspipe
             line = process.stdout.readline() # type: ignore
-            if line.startswith("$"):
+            if line.startswith('$'):
                 print(f"{checksum(line.strip())} : {line.strip()}")
             else:
                 break
     except KeyboardInterrupt:
         print("Stopping GPS data read...")
     finally:
+        # Clean up the process
         process.terminate()
         process.wait()
 
