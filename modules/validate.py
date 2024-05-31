37b25dcd0091cde51348592db9694c57311eef79
Roy Betjes <roy.betjes@talpanetwork.com>
2024-06-01 00:15:44 +0200

    ...

--- modules/validate.py
+++ modules/validate.py
@@ -52,7 +52,7 @@ class VALIDATE:
                     line = process.stdout.readline()  # type: ignore
                     if line:
                         json_data = json.loads(line.strip())
-                        if json_data['class'] is 'DEVICE':
+                        if json_data['class'] == 'DEVICE':
                             devices = json_data.get('devices', [])
                             if devices:
                                 print(devices)
