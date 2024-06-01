92153309eb7978887543950e1a27c18b6069da1f
Roy Betjes <roy.betjes@talpanetwork.com>
2024-06-01 02:41:13 +0200

    ...

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from modules.validate import VALIDATE
 from modules.nmea import NMEA
 from modules.obd2 import OBD2
+import concurrent.futures
+
+def run_obd_tasks(obd2):
+    if obd2.start_obd():
+        obd2.get_speed()
+        obd2.get_rpm()
+        obd2.get_throttle_position()
+        obd2.get_engine_load()
+        obd2.get_coolant_temp()
+        obd2.get_fuel_pressure()
+        obd2.get_intake_temp()
+        obd2.get_run_time()
+        obd2.get_fuel_rate()
 
 if __name__ == "__main__":
     validate = VALIDATE()
     print(f"Validate result\t{bin(validate.result())}")
 
-nmea = NMEA()
-nmea.start_gps(insert=True)
+    nmea = NMEA()
+    obd2 = OBD2()
 
-obd2 = OBD2()
-if obd2.start_obd():
-    obd2.get_speed()
-    obd2.get_rpm()
-    obd2.get_throttle_position()
-    obd2.get_engine_load()
-    obd2.get_coolant_temp()
-    obd2.get_fuel_pressure()
-    obd2.get_intake_temp()
-    obd2.get_run_time()
-    obd2.get_fuel_rate()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(nmea.start_gps, True)
+        executor.submit(run_obd_tasks, obd2)
